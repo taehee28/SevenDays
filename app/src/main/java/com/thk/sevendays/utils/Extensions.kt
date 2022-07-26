64192c07a89f558b298649aa8ec190f0a7ce89fb
Kang Taehee <dev.thk28@gmail.com>
2022-07-26 17:10:44 +0900

    Utils.kt 파일을 Extensions.kt로 병합

--- app/src/main/java/com/thk/sevendays/utils/Extensions.kt
+++ app/src/main/java/com/thk/sevendays/utils/Extensions.kt
@@ -1,8 +1,19 @@
 package com.thk.sevendays.utils
 
+import androidx.navigation.NavController
+import com.thk.sevendays.SevenDaysScreen
 import java.time.LocalDate
+import java.time.temporal.ChronoUnit
 
 public val <T> List<T>.firstIndex: Int
     get() = if (this.isNotEmpty()) 0 else -1
 
 fun LocalDate.isToday() = this == LocalDate.now()
+
+fun LocalDate.challengingDaysFrom(startDate: LocalDate): Int {
+    return ChronoUnit.DAYS.between(startDate, this).toInt() + 1
+}
+
+fun NavController.navigateToDetail(id: String) {
+    navigate("${SevenDaysScreen.Detail.name}/$id")
+}
