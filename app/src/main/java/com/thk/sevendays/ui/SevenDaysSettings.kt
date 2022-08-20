fc2f6ac4dd92faaf51358ea1636bb943d87cf643
taehee28 <dev.thk28@gmail.com>
2022-08-20 17:36:14 +0900

    feat: SwitchPref 컴포저블 구현

--- app/src/main/java/com/thk/sevendays/ui/SevenDaysSettings.kt
+++ app/src/main/java/com/thk/sevendays/ui/SevenDaysSettings.kt
@@ -1,13 +1,15 @@
+
 package com.thk.sevendays.ui
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,6 +35,17 @@ fun SettingsScreen(
         }
     ) {
 
+        Column {
+            Section(title = "title") {
+                SwitchPref(text = "this is a switch", description = "description", checked = true, onCheckedChange = {})
+                SwitchPref(text = "this is a switch", description = "description", checked = false, onCheckedChange = {}, enabled = false)
+            }
+
+            Section(title = "title") {
+                SwitchPref(text = "this is a switch", checked = true, onCheckedChange = {})
+            }
+
+        }
     }
 }
 
@@ -66,3 +79,47 @@ fun Section(
 
     content()
 }
+
+@Composable
+fun SwitchPref(
+    text: String,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+    description: String = "",
+    enabled: Boolean = true
+) = Box(
+    modifier = Modifier
+        .fillMaxWidth()
+        .height(80.dp)
+) {
+
+    val textAlpha = if (enabled) 1f else 0.5f
+
+    Column(
+        modifier = Modifier.align(Alignment.CenterStart)
+    ) {
+        Text(
+            text = text,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier.alpha(textAlpha)
+        )
+        if (description.isNotBlank()) {
+            Text(
+                text = description,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                style = MaterialTheme.typography.caption.copy(fontWeight = FontWeight.Light),
+                modifier = Modifier.alpha(textAlpha)
+            )
+        }
+    }
+
+    Switch(
+        checked = checked,
+        onCheckedChange = onCheckedChange,
+        enabled = enabled,
+        modifier = Modifier.align(Alignment.CenterEnd)
+    )
+}
+
