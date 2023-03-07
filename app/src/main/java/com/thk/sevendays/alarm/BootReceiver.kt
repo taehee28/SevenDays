3b81e906c641b8c60a12e730fda71e7bd7fb2549
Kang Taehee <dev.thk28@gmail.com>
2023-03-07 10:56:06 +0900

    앱 알림 켜져있는 경우 재부팅 시 알람 재등록 기능 구현
    * BootReceiver에서 SettingsRepository와 ReminderAlarmManager 주입받아 알람 재등록

--- app/src/main/java/com/thk/sevendays/alarm/BootReceiver.kt
+++ app/src/main/java/com/thk/sevendays/alarm/BootReceiver.kt
@@ -6,6 +6,10 @@ import android.content.Intent
 import com.thk.data.logd
 import com.thk.data.repository.SettingsRepository
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -18,8 +22,12 @@ class BootReceiver: BroadcastReceiver() {
         requireNotNull(intent)
 
         if (intent.action == "android.intent.action.BOOT_COMPLETED") {
-            logd(">> BOOT COMPLETED-------------------------------------------------")
+            logd(">> BOOT COMPLETED")
 
+            CoroutineScope(Dispatchers.Default).launch {
+                val time = repository.getAlarmTime().first()
+                alarmManager.registerAlarm(time)
+            }
         }
     }
 }
